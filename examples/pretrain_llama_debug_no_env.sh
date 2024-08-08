96a6a89b62546ed2c11efe1f7e038350c3671eef
Evan Walters <evanatyourservice@gmail.com>
2024-08-08 14:46:51 -0600

    Create pretrain_llama_debug_no_env.sh

---
+++ examples/pretrain_llama_debug_no_env.sh
@@ -0,0 +1,45 @@
+#! /bin/bash
+
+export LIBTPU_INIT_ARGS='--xla_jf_spmd_threshold_for_windowed_einsum_mib=0 --xla_tpu_spmd_threshold_for_allgather_cse=10000 --xla_enable_async_all_gather=true --xla_tpu_enable_latency_hiding_scheduler=true TPU_MEGACORE=MEGACORE_DENSE'
+
+python -m EasyLM.models.llama.llama_train \
+    --mesh_dim='-1,1,1' \
+    --dtype='fp32' \
+    --total_steps=200000 \
+    --log_freq=250 \
+    --save_model_freq=1000000 \
+    --save_milestone_freq=1000000 \
+    --eval_steps 20 \
+    --load_llama_config='3b' \
+    --update_llama_config='' \
+    --load_dataset_state='' \
+    --load_checkpoint='' \
+    --optimizer.type='adamw' \
+    --optimizer.adamw_optimizer.weight_decay=0.1 \
+    --optimizer.adamw_optimizer.lr=3e-4 \
+    --optimizer.adamw_optimizer.end_lr=3e-12 \
+    --optimizer.adamw_optimizer.lr_warmup_steps=512 \
+    --optimizer.adamw_optimizer.lr_decay_steps=250000 \
+    --train_dataset.type='huggingface' \
+    --train_dataset.text_processor.fields='text' \
+    --train_dataset.huggingface_dataset.path='HuggingFaceFW/fineweb-edu' \
+    --train_dataset.huggingface_dataset.streaming=True \
+    --train_dataset.huggingface_dataset.seq_length=2048 \
+    --train_dataset.huggingface_dataset.batch_size=128 \
+    --train_dataset.huggingface_dataset.split='train' \
+    --train_dataset.huggingface_dataset.name='sample-100BT' \
+    --eval_dataset.type='huggingface' \
+    --eval_dataset.text_processor.fields='text' \
+    --eval_dataset.huggingface_dataset.path='HuggingFaceFW/fineweb-edu' \
+    --eval_dataset.huggingface_dataset.streaming=True \
+    --eval_dataset.huggingface_dataset.seq_length=2048 \
+    --eval_dataset.huggingface_dataset.batch_size=128 \
+    --eval_dataset.huggingface_dataset.split='train' \
+    --eval_dataset.huggingface_dataset.name='sample-10BT' \
+    --checkpointer.save_optimizer_state=True \
+    --logger.online=True \
+    --logger.prefix='EasyLM' \
+    --logger.project="open_llama_evan" \
+    --logger.output_dir="$HOME/bucket/eval_llm/hf_cache" \
+    --logger.wandb_dir="$HOME/bucket/eval_llm/hf_cache" \
+|& tee $HOME/output.txt
