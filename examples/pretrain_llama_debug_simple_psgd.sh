6282ea41b9d4ae9f39566c6436f1b71a8eb2888f
Evan Walters <evanatyourservice@gmail.com>
2024-08-10 15:35:31 -0600

    Update pretrain_llama_debug_simple_psgd.sh

--- examples/pretrain_llama_debug_simple_psgd.sh
+++ examples/pretrain_llama_debug_simple_psgd.sh
@@ -19,7 +19,7 @@ python -m EasyLM.models.llama.llama_train \
     --load_checkpoint='' \
     --optimizer.type='psgd' \
     --optimizer.psgd_optimizer.weight_decay=0.0001 \
-    --optimizer.psgd_optimizer.lr=3.0 \
+    --optimizer.psgd_optimizer.lr=1.0 \
     --optimizer.psgd_optimizer.lr_warmup_steps=512 \
     --optimizer.psgd_optimizer.lr_decay_steps=20000 \
     --optimizer.psgd_optimizer.b2=0.95 \
