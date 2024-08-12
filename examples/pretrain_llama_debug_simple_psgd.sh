521123962f6f729689dd6a77bfd2b52f688e2bbb
Evan Walters <evanatyourservice@gmail.com>
2024-08-12 14:05:42 -0600

    Update pretrain_llama_debug_simple_psgd.sh

--- examples/pretrain_llama_debug_simple_psgd.sh
+++ examples/pretrain_llama_debug_simple_psgd.sh
@@ -10,15 +10,15 @@ python -m EasyLM.models.llama.llama_train \
     --save_model_freq=1000000 \
     --save_milestone_freq=1000000 \
     --eval_steps 5 \
-    --calc_hessian=False \
+    --calc_hessian=True \
     --update_prob=0.1 \
-    --l2_reg=0.0 \
+    --l2_reg=0.0001 \
     --load_llama_config='3b' \
     --update_llama_config='' \
     --load_dataset_state='' \
     --load_checkpoint='' \
     --optimizer.type='psgd' \
-    --optimizer.psgd_optimizer.weight_decay=0.001 \
+    --optimizer.psgd_optimizer.weight_decay=0.0001 \
     --optimizer.psgd_optimizer.lr=0.01 \
     --optimizer.psgd_optimizer.lr_warmup_steps=512 \
     --optimizer.psgd_optimizer.lr_decay_steps=20000 \
@@ -26,7 +26,7 @@ python -m EasyLM.models.llama.llama_train \
     --optimizer.psgd_optimizer.b2=0.95 \
     --optimizer.psgd_optimizer.clip_gradient=1.0 \
     --optimizer.psgd_optimizer.nesterov=True \
-    --optimizer.psgd_optimizer.precond_update_probability=0.2 \
+    --optimizer.psgd_optimizer.precond_update_probability=0.1 \
     --optimizer.psgd_optimizer.precond_lr=0.01 \
     --optimizer.psgd_optimizer.precond_init_scale=0.0 \
     --optimizer.psgd_optimizer.max_size_triangular=1024 \
