4b12e20d195447a5c6c23e9f5f9d61c5c9053092
Evan Walters <evanatyourservice@gmail.com>
2024-08-15 10:55:52 -0600

    Update psgd_affine.py

--- EasyLM/optimizers/psgd_affine.py
+++ EasyLM/optimizers/psgd_affine.py
@@ -205,13 +205,6 @@ def scale_by_affine(
             vector,
         )
 
-        # momentum
-        mu = None
-        if state.mu is not None:
-            updates, mu = apply_momentum(updates, state.mu, count_inc, b1, nesterov)
-
-        vanilla_updates = updates
-
         # preconditioning
         flat_updates = [
             r[0](u) for u, r in zip(jax.tree.leaves(updates), affine_reshapers)
@@ -231,6 +224,11 @@ def scale_by_affine(
             )
 
         updates = jax.tree.map(lambda x: jnp.clip(x, -1.0, 1.0), updates)
+        
+        # momentum
+        mu = None
+        if state.mu is not None:
+            updates, mu = apply_momentum(updates, state.mu, count_inc, b1, nesterov)
 
         mu = otu.tree_cast(mu, mu_dtype)
         state = PSGDAffineState(count=count_inc, key=key, mu=mu, Qs=Qs)
